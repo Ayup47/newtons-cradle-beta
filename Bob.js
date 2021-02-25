49c28252727401ee9c2bd80acddf1e93a2a6c29e
Ayup47 <76904404+Ayup47@users.noreply.github.com>
2021-02-25 20:03:47 +0530

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,30 @@
+ class Bob {
+            constructor(x,y,r){
+              var options = {
+                  isStatic:false,
+                 restitution:2,
+                 density:1,
+                 friction:3
+               }  
+              this.x = x;
+              this.y = y;
+              this.r = r;
+              
+              this.body = Bodies.circle(this.x,this.y,(this.r-20)/2,options);
+              
+              World.add(world, this.body);
+            }
+            display(){
+                var paperPos = this.body.position;
+                
+                push();
+                translate(paperPos.x,paperPos.y);
+                rectMode(CENTER);
+                fill(255);
+                ellipse(0,0,this.r,this.r);
+                
+                pop();
+            }
+        
+    
+}
